4070a44956a3a94c36bdacc2094895170470dd0e
Suhaib Maree <suhaib.maree@exalt.ps>
2021-09-22 12:28:16 +0300

    - Add Header

---
+++ src/components/Layout/header/Header.js
@@ -0,0 +1,20 @@
+import {Fragment} from 'react'
+import mealsImage from '../../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+
+    return <Fragment>
+        <header className={classes.header}>
+            <h1 className={classes['header-title']}>Pal Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='meals'/>
+        </div>
+    </Fragment>;
+
+}
+
+export default Header;
