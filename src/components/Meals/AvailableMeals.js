da29c2ff9ce16845733fef3558f4c59700dde919
Suhaib Maree <suhaib.maree@exalt.ps>
2021-09-22 16:51:42 +0300

    - Add meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css'
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -29,9 +31,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
     return <section className={classes.meals}>
-        <ul>
-            {DUMMY_MEALS.map(value => <li>{value.name}</li>)}
-        </ul>
+        <Card>
+            <ul>
+                {DUMMY_MEALS.map(meal =>
+                    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
+                )}
+            </ul>
+        </Card>
     </section>
 }
 
